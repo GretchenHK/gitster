cf528b5051e51e6323158e74ba1bf26a79ba5227
Gretchen Halasi-Kun <g.halasikun@gmail.com>
2015-10-14 09:58:28 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gitster::Application.config.secret_key_base = '1e9320add84a9b133d2f6027626c5fba1395edea82dd90b40d0b9b8e66ef7a3cc6c19fe2ca6ada05c4630d56e6e138ef268b3da4117de7a5a2bd73058bba8954'
